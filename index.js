2a1e398c5b5b153cf4fdb75d99dc8445053c66e0
Adam Gall <adam@decentlabs.io>
2020-03-23 13:17:48 -0400

    Move all the mailchimp logic into a new "mailchimp.js" file

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const validator = require('validator');
-const Mailchimp = require('mailchimp-api-v3');
 
 const { codes, respond } = require('./responses');
 
@@ -24,30 +23,4 @@ exports.handler = (event, _, callback) => {
     respond(codes.success, email, callback);
     return;
   }
-
-  const mailchimpApiKey = event.stageVariables.mailchimp_api_key;
-  const mailchimpListId = event.stageVariables.mailchimp_list_id;
-
-  if (!mailchimpApiKey || !mailchimpListId) {
-    respond(codes.error.internal.badConfig, email, callback);
-    return;
-  }
-
-  const postBody = { email_address: email, status: 'subscribed' };
-  const interestId = event.stageVariables.mailchimp_interest_id;
-  if (interestId) postBody.interests = { [interestId]: true };
-
-  const mailchimp = new Mailchimp(mailchimpApiKey);
-  mailchimp.post(`/lists/${mailchimpListId}/members`, postBody).then(function (results) {
-    console.log(results);
-    respond(codes.success, email, callback);
-    return;
-  }).catch(function (err) {
-    console.log(err);
-    if (err.title === "Member Exists") {
-      respond(codes.error.mailchimp.alreadySubscribed, email, callback);
-      return;
-    }
-    respond(codes.error.internal.unknown, email, callback);
-  });
 };
---
+++ mailchimp.js
@@ -0,0 +1,30 @@
+const Mailchimp = require('mailchimp-api-v3');
+
+const mailchimp = (vars, email) => {
+  const mailchimpApiKey = vars.mailchimp_api_key;
+  const mailchimpListId = vars.mailchimp_list_id;
+
+  if (!mailchimpApiKey || !mailchimpListId) return;
+
+  const postBody = { email_address: email, status: 'subscribed' };
+  const interestId = vars.mailchimp_interest_id;
+  if (interestId) postBody.interests = { [interestId]: true };
+
+  const mailchimp = new Mailchimp(mailchimpApiKey);
+
+  const promise = new Promise(resolve => {
+    return mailchimp.post(`/lists/${mailchimpListId}/members`, postBody).then(results => {
+      console.log('mailchimp success:', results);
+      resolve();
+      return;
+    }).catch(err => {
+      console.log('mailchimp error:', err);
+      resolve();
+      return;
+    });
+  });
+
+  return promise;
+}
+
+module.exports = { mailchimp };
